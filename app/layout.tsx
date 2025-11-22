b7cbade92e1509a6906bff85f842581454018981
lucasmmg12 <lucas@growsanjuan.com>
2025-11-22 17:44:02 -0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Liquidaciones Médicas | Grow Labs',
   description: 'Sistema profesional de liquidaciones médicas para todas las obras sociales y gestión de instrumentadores - Powered by Grow Labs',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
